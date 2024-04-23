a3df3860bae25ac94eead9eb5ac57692d3d3fc32
zenglsh <996433583@qq.com>
2024-04-23 15:13:54 +0800

    fix:代码调整

--- e-commerce-alibaba-nacos-client/src/main/java/com/imooc/ecommerce/controller/NacosClientController.java
+++ e-commerce-alibaba-nacos-client/src/main/java/com/imooc/ecommerce/controller/NacosClientController.java
@@ -18,6 +18,7 @@ import java.util.List;
 @Slf4j
 @RestController
 @RequestMapping("/nacos-client")
+@SuppressWarnings("all")
 public class NacosClientController {
 
     private final NacosClientService nacosClientService;
